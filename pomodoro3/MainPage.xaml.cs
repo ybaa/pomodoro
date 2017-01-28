7390019302108016789f7f2d7a6f824e4a6afde2
Martyna Łagożna <m.lagozna@live.com>
2017-01-28 13:35:09 +0100

    icon and tile added

--- pomodoro3/MainPage.xaml.cs
+++ pomodoro3/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
-namespace pomodoro3 {
+namespace pomodoro {
 
     public sealed partial class MainPage : Page
     {
